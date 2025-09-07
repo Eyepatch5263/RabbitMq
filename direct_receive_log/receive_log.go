7803d142beddc4ff10d47739fc938f6f416e49cd
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-09-07 14:35:54 +0530

    implemeneted direct type exchange

--- direct_receive_log/receive_log.go
+++ direct_receive_log/receive_log.go
@@ -2,11 +2,10 @@ package main
 
 import (
         "log"
-		
+        "os"
+
         amqp "github.com/rabbitmq/amqp091-go"
 )
-
-// custom error function
 
 func failOnError(err error, msg string) {
         if err != nil {
@@ -15,33 +14,25 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-		// create a connection
-
         conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
         failOnError(err, "Failed to connect to RabbitMQ")
         defer conn.Close()
-
-		// create a channel
 
         ch, err := conn.Channel()
         failOnError(err, "Failed to open a channel")
         defer ch.Close()
 
-		// create an exchange
-
         err = ch.ExchangeDeclare(
-                "logs",   // name
-                "fanout", // type
-                true,     // durable
-                false,    // auto-deleted
-                false,    // internal
-                false,    // no-wait
-                nil,      // arguments
+                "logs_direct", // name
+                "direct",      // type
+                true,          // durable
+                false,         // auto-deleted
+                false,         // internal
+                false,         // no-wait
+                nil,           // arguments
         )
         failOnError(err, "Failed to declare an exchange")
 
-		// declare a queue
-		// since queue name ie empty the server will itself give it a random name
         q, err := ch.QueueDeclare(
                 "",    // name
                 false, // durable
@@ -52,26 +43,29 @@ func main() {
         )
         failOnError(err, "Failed to declare a queue")
 
-		// bind the queue with an exchange
-
-        err = ch.QueueBind(
-                q.Name, // queue name
-                "",     // routing key
-                "logs", // exchange
-                false,
-                nil,
-        )
-        failOnError(err, "Failed to bind a queue")
-
-		// consume the queue
+        if len(os.Args) < 2 {
+                log.Printf("Usage: %s [info] [warning] [error]", os.Args[0])
+                os.Exit(0)
+        }
+        for _, s := range os.Args[1:] {
+                log.Printf("Binding queue %s to exchange %s with routing key %s",
+                        q.Name, "logs_direct", s)
+                err = ch.QueueBind(
+                        q.Name,        // queue name
+                        s,             // routing key
+                        "logs_direct", // exchange
+                        false,
+                        nil)
+                failOnError(err, "Failed to bind a queue")
+        }
 
         msgs, err := ch.Consume(
                 q.Name, // queue
                 "",     // consumer
-                true,   // auto-ack
+                true,   // auto ack
                 false,  // exclusive
-                false,  // no-local
-                false,  // no-wait
+                false,  // no local
+                false,  // no wait
                 nil,    // args
         )
         failOnError(err, "Failed to register a consumer")
